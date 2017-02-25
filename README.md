8da8331d08e5c594f4b2e0f07794f2fcb3d3a5f4
Junki Ishida <jishida.contact@gmail.com>
2017-02-25 17:37:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# print-jvm-props
+
+### Usage
+
+setup
+```
+$ git clone https://github.com/jishida/pjp.git
+$ source pjp/bin/pjpenv
+```
+
+print properties
+```
+$ pjp os.name os.arch
+os.name=Linux
+os.arch=amd64
+```
+
+print all properties
+```
+$ pjp
+awt.toolkit=sun.awt.X11.XToolkit
+file.encoding=ISO-8859-1
+file.encoding.pkg=sun.io
+file.separator=/
+java.awt.graphicsenv=sun.awt.X11GraphicsEnvironment
+java.awt.printerjob=sun.print.PSPrinterJob
+java.class.path=.
+java.class.version=52.0
+...
+```
---
+++ lib/pjp.java
@@ -0,0 +1,32 @@
+import java.util.Enumeration;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class pjp {
+	public static void main (String[] args) {
+		final List names = new ArrayList();
+		if(args.length == 0){
+			final Enumeration e = System.getProperties().propertyNames();
+			while(e.hasMoreElements()) {
+				names.add(e.nextElement().toString());
+			}
+			Collections.sort(names);
+		} else {		
+			for (int i = 0; i < args.length; i++) {
+				names.add(args[i]);
+			}
+		}
+		final Iterator iter = names.iterator();
+		while (iter.hasNext()) {
+			printProperty(iter.next().toString());
+		}
+	}
+	
+	private static void printProperty(final String name) {
+		final String prop = System.getProperty(name);
+		final String output = name + "=" + (prop == null ? "<null>" : prop);
+		System.out.println(output);
+	}
+}
